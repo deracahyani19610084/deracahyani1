47ac48ec8302b7d7105ea4a61cdb5e10324d105e
deracahyani19610084 <90818673+deracahyani19610084@users.noreply.github.com>
2021-12-21 10:10:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -49,7 +49,7 @@
                 } ?>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Home</a>
+                <a class="nav-link" href="index.html">Home</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="shop.html">Shop</a>
